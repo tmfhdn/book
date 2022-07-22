579faefd5aad75efaf626fa3cc0aa58cc567491e
tmfhdn <104114503+tmfhdn@users.noreply.github.com>
2022-07-22 13:42:43 +0900

    07/22

--- js/bb.js
+++ js/bb.js
@@ -71,12 +71,12 @@ $.ajax({
     headers: { Authorization: "KakaoAK bd0657efca6e474c953ac7a43b6a08df" }
 })
     .done(function (msg) {
-        var genres = document.getElementsByClassName("genre1");
+        var genres = document.getElementsByClassName("item5-2");
         for (var i = 0; i < genres.length; i++) {
-            $("ul > .genre1").eq(i).append("<img src='" + msg.documents[i].thumbnail + "'/>");
-            $("ul > .genre1").eq(i).append("<h4>" + msg.documents[i].title + "</h4>");
-            $("ul > .genre1").eq(i).append("<h5>" + msg.documents[i].authors + "</h5>");
-            $("ul > .genre1").eq(i).append("<h5>" + msg.documents[i].publisher + "</h5>");
+            $("ul > .item5-2").eq(i).append("<img src='" + msg.documents[i].thumbnail + "'/>");
+            $("ul > .item5-2").eq(i).append("<h4>" + msg.documents[i].title + "</h4>");
+            $("ul > .item5-2").eq(i).append("<h5>" + msg.documents[i].authors + "</h5>");
+            $("ul > .item5-2").eq(i).append("<h5>" + msg.documents[i].publisher + "</h5>");
         }
     });
 
@@ -87,12 +87,12 @@ $.ajax({
     headers: { Authorization: "KakaoAK bd0657efca6e474c953ac7a43b6a08df" }
 })
     .done(function (msg) {
-        var genres = document.getElementsByClassName("genre2");
+        var genres = document.getElementsByClassName("item5-3");
         for (var i = 0; i < genres.length; i++) {
-            $("ul > .genre2").eq(i).append("<img src='" + msg.documents[i].thumbnail + "'/>");
-            $("ul > .genre2").eq(i).append("<h4>" + msg.documents[i].title + "</h4>");
-            $("ul > .genre2").eq(i).append("<h5>" + msg.documents[i].authors + "</h5>");
-            $("ul > .genre2").eq(i).append("<h5>" + msg.documents[i].publisher + "</h5>");
+            $("ul > .item5-3").eq(i).append("<img src='" + msg.documents[i].thumbnail + "'/>");
+            $("ul > .item5-3").eq(i).append("<h4>" + msg.documents[i].title + "</h4>");
+            $("ul > .item5-3").eq(i).append("<h5>" + msg.documents[i].authors + "</h5>");
+            $("ul > .item5-3").eq(i).append("<h5>" + msg.documents[i].publisher + "</h5>");
         }
     });
 
@@ -103,12 +103,12 @@ $.ajax({
     headers: { Authorization: "KakaoAK bd0657efca6e474c953ac7a43b6a08df" }
 })
     .done(function (msg) {
-        var genres = document.getElementsByClassName("genre3");
+        var genres = document.getElementsByClassName("item5-4");
         for (var i = 0; i < genres.length; i++) {
-            $("ul > .genre3").eq(i).append("<img src='" + msg.documents[i].thumbnail + "'/>");
-            $("ul > .genre3").eq(i).append("<h4>" + msg.documents[i].title + "</h4>");
-            $("ul > .genre3").eq(i).append("<h5>" + msg.documents[i].authors + "</h5>");
-            $("ul > .genre3").eq(i).append("<h5>" + msg.documents[i].publisher + "</h5>");
+            $("ul > .item5-4").eq(i).append("<img src='" + msg.documents[i].thumbnail + "'/>");
+            $("ul > .item5-4").eq(i).append("<h4>" + msg.documents[i].title + "</h4>");
+            $("ul > .item5-4").eq(i).append("<h5>" + msg.documents[i].authors + "</h5>");
+            $("ul > .item5-4").eq(i).append("<h5>" + msg.documents[i].publisher + "</h5>");
         }
     });
 
@@ -119,12 +119,12 @@ $.ajax({
     headers: { Authorization: "KakaoAK bd0657efca6e474c953ac7a43b6a08df" }
 })
     .done(function (msg) {
-        var genres = document.getElementsByClassName("genre4");
+        var genres = document.getElementsByClassName("item5-5");
         for (var i = 0; i < genres.length; i++) {
-            $("ul > .genre4").eq(i).append("<img src='" + msg.documents[i].thumbnail + "'/>");
-            $("ul > .genre4").eq(i).append("<h4>" + msg.documents[i].title + "</h4>");
-            $("ul > .genre4").eq(i).append("<h5>" + msg.documents[i].authors + "</h5>");
-            $("ul > .genre4").eq(i).append("<h5>" + msg.documents[i].publisher + "</h5>");
+            $("ul > .item5-5").eq(i).append("<img src='" + msg.documents[i].thumbnail + "'/>");
+            $("ul > .item5-5").eq(i).append("<h4>" + msg.documents[i].title + "</h4>");
+            $("ul > .item5-5").eq(i).append("<h5>" + msg.documents[i].authors + "</h5>");
+            $("ul > .item5-5").eq(i).append("<h5>" + msg.documents[i].publisher + "</h5>");
         }
     });
 
@@ -135,48 +135,49 @@ $.ajax({
     headers: { Authorization: "KakaoAK bd0657efca6e474c953ac7a43b6a08df" }
 })
     .done(function (msg) {
-        var genres = document.getElementsByClassName("genre5");
+        var genres = document.getElementsByClassName("item5-6");
         for (var i = 0; i < genres.length; i++) {
-            $("ul > .genre5").eq(i).append("<img src='" + msg.documents[i].thumbnail + "'/>");
-            $("ul > .genre5").eq(i).append("<h4>" + msg.documents[i].title + "</h4>");
-            $("ul > .genre5").eq(i).append("<h5>" + msg.documents[i].authors + "</h5>");
-            $("ul > .genre5").eq(i).append("<h5>" + msg.documents[i].publisher + "</h5>");
+            $("ul > .item5-6").eq(i).append("<img src='" + msg.documents[i].thumbnail + "'/>");
+            $("ul > .item5-6").eq(i).append("<h4>" + msg.documents[i].title + "</h4>");
+            $("ul > .item5-6").eq(i).append("<h5>" + msg.documents[i].authors + "</h5>");
+            $("ul > .item5-6").eq(i).append("<h5>" + msg.documents[i].publisher + "</h5>");
         }
     });
 
+
 $(function () {
 
-    $(".genre>button:nth-of-type(1)").click(function () {
+    $(".item5-1>button:nth-of-type(1)").click(function () {
         $(this).addClass("btn_on");
-        $(".genre>button:nth-of-type(2), .genre>button:nth-of-type(3), .genre>button:nth-of-type(4), .genre>button:nth-of-type(5)").removeClass("btn_on");
-        $(".genre1").fadeIn(250).css("zIndex", "1");
-        $(".genre2, .genre3, .genre4, .genre5").fadeOut(250);
+        $(".item5-1>button:nth-of-type(2), .item5-1>button:nth-of-type(3), .item5-1>button:nth-of-type(4), .item5-1>button:nth-of-type(5)").removeClass("btn_on");
+        $(".item5-2").fadeIn(250).css("zIndex", "1");
+        $(".item5-3, .item5-4, .item5-5, .item5-6").fadeOut(250);
     })
-    $(".genre>button:nth-of-type(2)").click(function () {
+    $(".item5-1>button:nth-of-type(2)").click(function () {
         $(this).addClass("btn_on");
-        $(".genre>button:nth-of-type(1), .genre>button:nth-of-type(3), .genre>button:nth-of-type(4), .genre>button:nth-of-type(5)").removeClass("btn_on");
-        $(".genre2").fadeIn(250).css("zIndex", "1");
-        $(".genre1, .genre3, .genre4, .genre5").fadeOut(250);
+        $(".item5-1>button:nth-of-type(1), .item5-1>button:nth-of-type(3), .item5-1>button:nth-of-type(4), .item5-1>button:nth-of-type(5)").removeClass("btn_on");
+        $(".item5-3").fadeIn(250).css("zIndex", "1");
+        $(".item5-2, .item5-4, .item5-5, .item5-6").fadeOut(250);
     })
-    $(".genre>button:nth-of-type(3)").click(function () {
+    $(".item5-1>button:nth-of-type(3)").click(function () {
         $(this).addClass("btn_on");
-        $(".genre>button:nth-of-type(1), .genre>button:nth-of-type(2), .genre>button:nth-of-type(4), .genre>button:nth-of-type(5)").removeClass("btn_on");
-        $(".genre3").fadeIn(250).css("zIndex", "1");
-        $(".genre1, .genre2, .genre4, .genre5").fadeOut(250);
+        $(".item5-1>button:nth-of-type(1), .item5-1>button:nth-of-type(2), .item5-1>button:nth-of-type(4), .item5-1>button:nth-of-type(5)").removeClass("btn_on");
+        $(".item5-4").fadeIn(250).css("zIndex", "1");
+        $(".item5-2, .item5-3, .item5-5, .item5-6").fadeOut(250);
     })
-    $(".genre>button:nth-of-type(4)").click(function () {
+    $(".item5-1>button:nth-of-type(4)").click(function () {
         $(this).addClass("btn_on");
-        $(".genre>button:nth-of-type(1), .genre>button:nth-of-type(2), .genre>button:nth-of-type(3), .genre>button:nth-of-type(5)").removeClass("btn_on");
-        $(".genre4").fadeIn(250).css("zIndex", "1");
-        $(".genre1, .genre2, .genre3, .genre5").fadeOut(250);
+        $(".item5-1>button:nth-of-type(1), .item5-1>button:nth-of-type(2), .item5-1>button:nth-of-type(3), .item5-1>button:nth-of-type(5)").removeClass("btn_on");
+        $(".item5-5").fadeIn(250).css("zIndex", "1");
+        $(".item5-2, .item5-3, .item5-4, .item5-6").fadeOut(250);
     })
-    $(".genre>button:nth-of-type(5)").click(function () {
+    $(".item5-1>button:nth-of-type(5)").click(function () {
         $(this).addClass("btn_on");
-        $(".genre>button:nth-of-type(1), .genre>button:nth-of-type(2), .genre>button:nth-of-type(3), .genre>button:nth-of-type(4)").removeClass("btn_on");
-        $(".genre5").fadeIn(250).css("zIndex", "1");
-        $(".genre1, .genre2, .genre3, .genre4").fadeOut(250);
+        $(".item5-1>button:nth-of-type(1), .item5-1>button:nth-of-type(2), .item5-1>button:nth-of-type(3), .item5-1>button:nth-of-type(4)").removeClass("btn_on");
+        $(".item5-6").fadeIn(250).css("zIndex", "1");
+        $(".item5-2, .item5-3, .item5-4, .item5-5").fadeOut(250);
     })
-
+// ---------------------------------------------------------------------------------------------------------------
     $('.qna1').click(function () {
         $('.q1').toggleClass('q_on');
         $('.a1').slideToggle(250);
@@ -214,39 +215,39 @@ $(function () {
 
 
 
-//tap click
-document.querySelector('.tap1').addEventListener('click', function () {
+
+document.querySelector('.item8-1-1').addEventListener('click', function () {
     this.classList.add('tap_on');
-    document.querySelector('.tap2').classList.remove('tap_on');
-    document.querySelector('.tap3').classList.remove('tap_on');
+    document.querySelector('.item8-1-2').classList.remove('tap_on');
+    document.querySelector('.item8-1-3').classList.remove('tap_on');
     document.querySelector('.cw_1').style.display = "block";
     document.querySelector('.cw_2').style.display = "none";
     document.querySelector('.cw_3').style.display = "none";
 });
-document.querySelector('.tap2').addEventListener('click', function () {
+document.querySelector('.item8-1-2').addEventListener('click', function () {
     this.classList.add('tap_on');
-    document.querySelector('.tap1').classList.remove('tap_on');
-    document.querySelector('.tap3').classList.remove('tap_on');
+    document.querySelector('.item8-1-1').classList.remove('tap_on');
+    document.querySelector('.item8-1-3').classList.remove('tap_on');
     document.querySelector('.cw_2').style.display = "block";
     document.querySelector('.cw_1').style.display = "none";
     document.querySelector('.cw_3').style.display = "none";
 });
-document.querySelector('.tap3').addEventListener('click', function () {
+document.querySelector('.item8-1-3').addEventListener('click', function () {
     this.classList.add('tap_on');
-    document.querySelector('.tap1').classList.remove('tap_on');
-    document.querySelector('.tap2').classList.remove('tap_on');
+    document.querySelector('.item8-1-1').classList.remove('tap_on');
+    document.querySelector('.item8-1-2').classList.remove('tap_on');
     document.querySelector('.cw_3').style.display = "block";
     document.querySelector('.cw_1').style.display = "none";
     document.querySelector('.cw_2').style.display = "none";
 });
 
-//phone screen slide contents_wrap1
+
 document.querySelector('.cw_1 .screen_btn1').addEventListener('click', function () {
     this.classList.add('btn_on');
     document.querySelector('.cw_1 .screen_btn2').classList.remove('btn_on');
     document.querySelector('.cw_1 .screen_btn3').classList.remove('btn_on');
     document.querySelector('.cw_1 .screen_btn4').classList.remove('btn_on');
-    document.querySelector('.cw_1 .phone_wrap').style.transform = 'translate(-0px)';
+    document.querySelector('.cw_1 .item8-5-1').style.transform = 'translate(-0px)';
     document.querySelector('.cw_1 .ment').innerHTML = "<b>하루의 시작과 마무리를 오디오북과 함께</b> <br> 지하철에서도, 잠들기 전에도 독서하세요";
 });
 
@@ -255,7 +256,7 @@ document.querySelector('.cw_1 .screen_btn2').addEventListener('click', function 
     document.querySelector('.cw_1 .screen_btn1').classList.remove('btn_on');
     document.querySelector('.cw_1 .screen_btn3').classList.remove('btn_on');
     document.querySelector('.cw_1 .screen_btn4').classList.remove('btn_on');
-    document.querySelector('.cw_1 .phone_wrap').style.transform = 'translate(-330px)';
+    document.querySelector('.cw_1 .item8-5-1').style.transform = 'translate(-330px)';
     document.querySelector('.cw_1 .ment').innerHTML = "<b>귀로 듣다가 눈으로도 같이 보면</b> <br> 더 내용이 잘 이해돼요";
 });
 
@@ -264,7 +265,7 @@ document.querySelector('.cw_1 .screen_btn3').addEventListener('click', function 
     document.querySelector('.cw_1 .screen_btn1').classList.remove('btn_on');
     document.querySelector('.cw_1 .screen_btn2').classList.remove('btn_on');
     document.querySelector('.cw_1 .screen_btn4').classList.remove('btn_on');
-    document.querySelector('.cw_1 .phone_wrap').style.transform = 'translate(-660px)';
+    document.querySelector('.cw_1 .item8-5-1').style.transform = 'translate(-660px)';
     document.querySelector('.cw_1 .ment').innerHTML = "<b>30분만에 책 요약</b> <br> 핵심만 쏙 골라 읽어주는 오디오북";
 });
 
@@ -273,17 +274,17 @@ document.querySelector('.cw_1 .screen_btn4').addEventListener('click', function 
     document.querySelector('.cw_1 .screen_btn1').classList.remove('btn_on');
     document.querySelector('.cw_1 .screen_btn2').classList.remove('btn_on');
     document.querySelector('.cw_1 .screen_btn3').classList.remove('btn_on');
-    document.querySelector('.cw_1 .phone_wrap').style.transform = 'translate(-990px)';
+    document.querySelector('.cw_1 .item8-5-1').style.transform = 'translate(-990px)';
     document.querySelector('.cw_1 .ment').innerHTML = "<b>이제훈부터 옥주현까지</b> <br> 셀럽이 읽어주는 오디오북";
 });
 
-//phone screen slide contents_wrap2
+
 document.querySelector('.cw_2 .screen_btn1').addEventListener('click', function () {
     this.classList.add('btn_on');
     document.querySelector('.cw_2 .screen_btn2').classList.remove('btn_on');
     document.querySelector('.cw_2 .screen_btn3').classList.remove('btn_on');
     document.querySelector('.cw_2 .screen_btn4').classList.remove('btn_on');
-    document.querySelector('.cw_2 .phone_wrap').style.transform = 'translate(-0px)';
+    document.querySelector('.cw_2 .item8-5-1').style.transform = 'translate(-0px)';
     document.querySelector('.cw_2 .ment').innerHTML = "<b>시선 추적 기능</b> <br> 눈동자로 페이지를 넘길 수 있어요";
 });
 
@@ -292,7 +293,7 @@ document.querySelector('.cw_2 .screen_btn2').addEventListener('click', function 
     document.querySelector('.cw_2 .screen_btn1').classList.remove('btn_on');
     document.querySelector('.cw_2 .screen_btn3').classList.remove('btn_on');
     document.querySelector('.cw_2 .screen_btn4').classList.remove('btn_on');
-    document.querySelector('.cw_2 .phone_wrap').style.transform = 'translate(-330px)';
+    document.querySelector('.cw_2 .item8-5-1').style.transform = 'translate(-330px)';
     document.querySelector('.cw_2 .ment').innerHTML = "<b>어느 기기에서도 OK</b> <br> 스마트폰, 태블릿, PC, E ink 기기까지";
 });
 
@@ -301,7 +302,7 @@ document.querySelector('.cw_2 .screen_btn3').addEventListener('click', function 
     document.querySelector('.cw_2 .screen_btn1').classList.remove('btn_on');
     document.querySelector('.cw_2 .screen_btn2').classList.remove('btn_on');
     document.querySelector('.cw_2 .screen_btn4').classList.remove('btn_on');
-    document.querySelector('.cw_2 .phone_wrap').style.transform = 'translate(-660px)';
+    document.querySelector('.cw_2 .item8-5-1').style.transform = 'translate(-660px)';
     document.querySelector('.cw_2 .ment').innerHTML = "<b>다크 모드</b> <br> 전자책이 눈 아프다는 편견은 버려요";
 });
 
@@ -310,16 +311,16 @@ document.querySelector('.cw_2 .screen_btn4').addEventListener('click', function 
     document.querySelector('.cw_2 .screen_btn1').classList.remove('btn_on');
     document.querySelector('.cw_2 .screen_btn2').classList.remove('btn_on');
     document.querySelector('.cw_2 .screen_btn3').classList.remove('btn_on');
-    document.querySelector('.cw_2 .phone_wrap').style.transform = 'translate(-990px)';
+    document.querySelector('.cw_2 .item8-5-1').style.transform = 'translate(-990px)';
     document.querySelector('.cw_2 .ment').innerHTML = "<b>자유로운 보기 설정</b> <br> 줄간격부터 폰트까지 내 마음대로 설정하면 <br> 더욱 편한 독서를 할 수 있어요";
 });
 
-//phone screen slide contents_wrap3
+
 document.querySelector('.cw_3 .screen_btn1').addEventListener('click', function () {
     this.classList.add('btn_on');
     document.querySelector('.cw_3 .screen_btn2').classList.remove('btn_on');
     document.querySelector('.cw_3 .screen_btn3').classList.remove('btn_on');
-    document.querySelector('.cw_3 .phone_wrap').style.transform = 'translate(-0px)';
+    document.querySelector('.cw_3 .item8-5-1').style.transform = 'translate(-0px)';
     document.querySelector('.cw_3 .ment').innerHTML = "<b>당신을 위한 추천</b> <br> 키워드로 한 눈에 정리한 추천 도서";
 });
 
@@ -327,7 +328,7 @@ document.querySelector('.cw_3 .screen_btn2').addEventListener('click', function 
     this.classList.add('btn_on');
     document.querySelector('.cw_3 .screen_btn1').classList.remove('btn_on');
     document.querySelector('.cw_3 .screen_btn3').classList.remove('btn_on');
-    document.querySelector('.cw_3 .phone_wrap').style.transform = 'translate(-330px)';
+    document.querySelector('.cw_3 .item8-5-1').style.transform = 'translate(-330px)';
     document.querySelector('.cw_3 .ment').innerHTML = "<b>이럴 땐 이런 책</b> <br> 내 상황에 딱 맞는 회원들의 추천 책";
 });
 
@@ -335,6 +336,6 @@ document.querySelector('.cw_3 .screen_btn3').addEventListener('click', function 
     this.classList.add('btn_on');
     document.querySelector('.cw_3 .screen_btn1').classList.remove('btn_on');
     document.querySelector('.cw_3 .screen_btn2').classList.remove('btn_on');
-    document.querySelector('.cw_3 .phone_wrap').style.transform = 'translate(-660px)';
+    document.querySelector('.cw_3 .item8-5-1').style.transform = 'translate(-660px)';
     document.querySelector('.cw_3 .ment').innerHTML = "<b>책-잇</b> <br> 책을 통해 트렌드와 문해력을 한번에!";
 })
